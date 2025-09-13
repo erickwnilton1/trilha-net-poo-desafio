73a6c9b82c862ebf9c0486bdc7a92abf18b34a94
Erick Welberth <erickwelberth1@hotmail.com>
2025-09-13 18:15:39 -0300

    feat: realizar teste com a classe [iphone]

--- Program.cs
+++ Program.cs
@@ -4,3 +4,8 @@ Console.WriteLine("Nokia:");
 Smartphone nokia = new Nokia(numero: "9876", modelo: 8, imei: "3453752", memoria: 264);
 nokia.Ligar();
 nokia.InstalarAplicativo("github");
+
+Console.WriteLine("Iphone:");
+Smartphone iphone = new Iphone(numero: "9876", modelo: 16, imei: "3453752", memoria: 264);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
