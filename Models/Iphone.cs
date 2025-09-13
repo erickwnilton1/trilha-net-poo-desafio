084771342ef2d334753f3df894e6dcfb0b2dba15
Erick Welberth <erickwelberth1@hotmail.com>
2025-09-13 18:09:09 -0300

    feat: herdar classe e sobrescrever método [iphone]

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,13 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, int modelo, string imei, int memoria) : base(numero, modelo, imei, memoria){}
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            string Message = $"Instalando o aplicativo \"{nomeApp}\" no Iphone";
+            Console.WriteLine(Message);
+        }
     }
 }
